ce7b9e44c54ee63fb61acb61cc57de5c818b0f92
Piratheep Rajaratnam <piratheep@xdoto.io>
2023-11-09 16:53:04 +0530

    all updates

---
+++ src/Components/DatePicker/index.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { TextField, Button, FormLabel, Grid, CardContent } from "@mui/material";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import PropTypes from "prop-types";
+import CardModel from "../CardModel";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
+
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+function App({ setDataState,dataState,data,btnName,Add1,Add2,Index }) {
+  const [singleCard, setSingleCard] = useState(true);
+  const [openCard, setOpenCard] = useState(false);
+  const [dateState, setDateState] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+
+  const handleClickOpenCard = () => {
+    setOpenCard(true);
+  };
+
+  const handleCloseCard = () => {
+    setOpenCard(false);
+  };
+
+  return (
+    <Box>
+      <Box display={"flex"} justifyContent={"space-between"} p={2}>
+        {/* <Box display={"flex"} sx={{}}>
+          <CalendarMonthIcon />{" "}
+          {dateState.toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+          })}
+          <AccessTimeFilledIcon />
+          {dateState.toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          })}
+        </Box> */}
+        <CardModel
+          setOpenCard={setOpenCard}
+          open={openCard}
+          handleClose={handleCloseCard}
+          singleCard={singleCard}
+          setDataState={setDataState}
+          dataState={dataState}
+          Add1={Add1}
+          Add2={Add2}
+          Index={Index}
+          
+          // AddWidgetData={data}
+        />
+
+        <Button variant="contained" onClick={handleClickOpenCard}>
+          {btnName}
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
